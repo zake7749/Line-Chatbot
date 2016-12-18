184f02ef916c8000bcdf1a8e5e2522cf6327771d
zake7749 <zake7749@gmail.com>
2016-12-18 13:52:58 +0800

    Add lineweatherbot.herokuapp.com to ALLOWED_HOST.

--- line_bot/settings.py
+++ line_bot/settings.py
@@ -29,7 +29,9 @@ LINE_CHANNEL_SECRET = get_env_variable("LINE_CHANNEL_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lineweatherbot.herokuapp.com'
+]
 
 
 # Application definition
